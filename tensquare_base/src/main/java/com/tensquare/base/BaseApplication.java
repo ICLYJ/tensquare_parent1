a60e9b9740fc25f2e22570df64da0e2be70c78fa
e <1421805850@qq.com>
2019-02-27 15:38:50 +0800

    初始化

---
+++ tensquare_base/src/main/java/com/tensquare/base/BaseApplication.java
@@ -0,0 +1,25 @@
+package com.tensquare.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import util.IdWorker;
+
+/**
+ * Created by lyj on 2019/1/19.
+ */
+@SpringBootApplication
+@CrossOrigin //跨域
+public class BaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseApplication.class);
+    }
+
+    @Bean
+    public IdWorker idWorker(){
+        return new IdWorker(1,1);
+    }
+
+}
---
+++ tensquare_rabbitmqdemo/src/main/java/tensquare/rabbit/customer/Customer2.java
@@ -0,0 +1,19 @@
+package tensquare.rabbit.customer;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by lyj on 2019/1/21.
+ */
+@Component
+@RabbitListener(queues = "itheima")
+public class Customer2 {
+
+    @RabbitHandler
+    public void showMessage(String message){
+        System.out.println("itheima："+ message);
+    }
+
+}
