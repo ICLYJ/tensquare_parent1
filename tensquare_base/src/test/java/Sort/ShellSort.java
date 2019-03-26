a05f1c9e1cec1525ba2d2f0f7874a3c5b3d3b7a7
c_shazhengnian <1421805850@qq.com>
2019-03-26 10:34:27 +0800

    修改注释

--- tensquare_base/src/test/java/Sort/ShellSort.java
+++ tensquare_base/src/test/java/Sort/ShellSort.java
@@ -10,6 +10,17 @@ import java.util.Random;
  */
 public class ShellSort {
 
+    /**
+     * 思想：基于插入排序，交换不相邻的元素已对数组的局部进行排序，
+     * 并最终用插入排序将局部有序的数组排序。
+     * 思想是使数组中任意间隔为h的元素都是有序的，这样的数组称为h有序数组.
+         特性：In-place sort，unstable sort。
+         思想：每次找一个最小值。
+         最好情况时间：O(n)。
+         最坏情况时间：O(n^2)。
+     * @param array
+     * @return
+     */
     public static int[] ShellSorts(int[] array){
         int len = array.length;
         int temp, gap = len / 2;
